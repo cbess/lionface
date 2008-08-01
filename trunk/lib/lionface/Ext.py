c3a9ba2ce3d4e1b4e4959f630dbcab8c6ff50191
cbbess <cbbess@f21d62ee-ed4e-0410-9bfc-33f004d4b02c>
2008-08-01 05:42:18 +0000

    added new files, updated qucomm to implement django is_ajax headers

---
+++ trunk/lib/lionface/Ext.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Ext.py
+
+implements the ExtJS ui classes
+
+Created by Christopher Bess on 2008-05-19.
+Copyright (c) 2008 Christopher Bess. All rights reserved.
+"""
+
+import ui
+
+class ExtBaseWidget(ui.Widget):
+    """Represents a base ExtJS widget"""
+    def __init__(self, id):
+        super(ExtBaseWidget, self).__init__(id)
+        
+
+class Window(ui.BaseWindow):
+    """Represents a Ext.Window instance """
+    def __init__(self, id):
+        super(Window, self).__init__(id)
+        pass
+        
+    def alignTo(self, id):
+        """align window to element
+        @param id: the id of the element
+        """
+        self.send_jscode(".alignTo('%s')" % (self.id, id))
+        pass
+        
+    def render(self):
+        """Renders this window"""
+        self._buffer = """
+        <div id="%s" class="x-hidden">
+    	    <div class="x-window-header">%s</div>
+    		<div class="x-window-body">
+    		    %s
+    	    </div>
+    	</div>
+        """ % (self.id, self.title, self.render_sub())
+        
+        return ui.BaseWindow.render(self)
